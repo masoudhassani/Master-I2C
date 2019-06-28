36f2bbfe366a3aeb6b27532a6477f9c1f4a9ccc6
masoudhassani <m.hasany@gmail.com>
2019-06-27 22:34:19 -0400

    added multi thread

--- Master-I2C.ino
+++ Master-I2C.ino
@@ -1,18 +1,24 @@
 #include <Wire.h>
+#include <Thread.h>
+#include <ThreadController.h>
 
 const int driverAddress = 0x01;
 const int masterAddress = 0x00;
 int8_t setpointAngle = 0;
+int8_t data[8];
 byte c;
 String incommingData = "";
+bool dir = false;
 
-void setup() {
-  Wire.begin(0x00); // join i2c bus (address optional for master)
-  Serial.begin(230400);
-}
+// controll that will controll all threads
+ThreadController multiThread = ThreadController();
 
+// initialize threads for multi tasking
+Thread commanderThread = Thread();    // to send commands
+Thread receiverThread = Thread();    // to receive data from motor
 
-void loop() {
+void sendCommand()
+{
     if (Serial.available() > 0){
         c = Serial.read();
         if (c != '\n'){
@@ -23,15 +29,61 @@ void loop() {
             incommingData = "";
         }
     }
-    //Serial.println(setpointAngle);
-    Wire.requestFrom(driverAddress, 8);   // request 8 bytes from device
-    while (Wire.available()) { // slave may send less than requested
-        uint8_t c = Wire.read(); // receive a byte as character
-        Serial.print(c);Serial.print('\t');        // print the character
+    else{
+        if (dir){
+            setpointAngle += 15;
+            if (setpointAngle > 14){
+                dir = false;
+            }
+        }
+        else{
+            setpointAngle -= 15;
+            if (setpointAngle < -14){
+                dir = true;
+            }
+        }
+
     }
-    Serial.print('\n');
+    //Serial.println(setpointAngle);
+
     Wire.beginTransmission(driverAddress); // transmit to device #8
     Wire.write(setpointAngle);              // sends one byte
     Wire.endTransmission();    // stop transmitting
-    delay(10);
+}
+
+void ReceiveData()
+{
+    uint8_t i = 0;
+    Wire.requestFrom(driverAddress, 8);   // request 8 bytes from device
+    while (Wire.available()) { // slave may send less than requested
+        int8_t c = Wire.read(); // receive a byte as character
+        data[i] = c;
+        i += 1;
+    }
+    Serial.print(data[0]);Serial.print('\t');Serial.print(data[1]);Serial.print('\t');
+    Serial.print('\n');
+}
+
+//StaticThreadController<2> controll (commanderThread, receiverThread);
+
+void setup() {
+    Wire.begin(0x00);        // join i2c bus
+    Serial.begin(230400);    // setup serial for debug
+
+    // setup threads
+    commanderThread.onRun(sendCommand);
+    commanderThread.setInterval(1000);
+
+	receiverThread.onRun(ReceiveData);
+	receiverThread.setInterval(10);
+
+    // Adds both threads to the controller
+	multiThread.add(&commanderThread);
+	multiThread.add(&receiverThread);
+}
+
+
+void loop()
+{
+    multiThread.run();
 }
