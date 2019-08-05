2c7f0f4bfcca08d3af1872ebb508b213d3c89ff7
masoudhassani <m.hasany@gmail.com>
2019-08-04 21:33:54 -0400

    fixed the data structure to read float

--- Master-I2C.ino
+++ Master-I2C.ino
@@ -11,14 +11,38 @@ const int masterAddress = 0x00;
 const int analogInPin   = A0;
 
 // i2c send/receive variables
-const uint8_t sizeOfData = 10;    // expected data from slave is 10 bytes. this might change
-int8_t data[sizeOfData];
-String command = "";
+const uint8_t sizeOfData = 10;    // expected data size from slave in number of bytes. this might change
+
+// ----------------------- data structure for motor data ---------------------
+/*
+define data structure for receiving data
+it creates a structure of 144 bites including four 32bit float,
+one 8bit integer and eight booleans.
+*/
+// data structure setup
+typedef struct motorData_t
+{
+    float    angle;
+    float    velocity;
+    float    acceleration;
+    float    current;
+    uint8_t  temperature;
+    uint8_t  booleans;
+};
+
+typedef union dataPackage_t
+{
+    motorData_t status;
+    byte dataPackage[sizeof(motorData_t)];
+};
+
+dataPackage_t motor;
 
 // master serial command variables
-byte c;
 String incommingData = "";
-bool dir = false;
+String command = "";
+int16_t setpointAngle;
+
 
 // controll that will controll all threads
 ThreadController multiThread = ThreadController();
@@ -31,9 +55,9 @@ Thread receiverThread = Thread();    // to receive data from motor
 void sendCommand()
 {
     if (Serial.available() > 0){
-        c = Serial.read();
+        char c = Serial.read();
         if (c != '\n'){
-            incommingData += (char)c;
+            incommingData += c;
         }
         else{
             command = incommingData;
@@ -52,28 +76,56 @@ void sendCommand()
         int16_t joyStick = analogRead(analogInPin);
 
         // map it to the range of the angle out
-        int8_t setpointAngle = map(joyStick, 0, 1024, -45, 45);
+        setpointAngle = map(joyStick, 0, 1024, -25, 25);
 
         // print the angle setpoint from analog input
-        Serial.print(setpointAngle);Serial.print('\t');
+        //Serial.print(setpointAngle);Serial.print('\t');
 
-        char cstr[4];
+        char cstr[5];
         Wire.beginTransmission(driverAddress);
         Wire.write(itoa(setpointAngle, cstr, 10));
         Wire.endTransmission();
     }
 }
 
+// function to receive motor driver data
 void ReceiveData()
 {
     uint8_t i = 0;
-    Wire.requestFrom(driverAddress, 8);   // request 8 bytes from device
-    while (Wire.available()) { // slave may send less than requested
-        int8_t c = Wire.read(); // receive a byte as character
-        data[i] = c;
+    byte buffer[sizeOfData];
+    Wire.requestFrom(driverAddress, sizeOfData);   // request sizeOfData bytes from device
+    while (Wire.available()) {    // slave may send less than requested
+        buffer[i] = Wire.read();  // receive a byte
         i += 1;
     }
-    Serial.print(data[0]);Serial.print('\t');Serial.print(data[1]);Serial.print('\n');
+
+    // recombine the separated 16bit data and assign it to corrent motor status variable
+    // assign 8bit motor status variables
+    int16_t angle = buffer[0];
+    angle = angle << 8 | buffer[1];
+    int16_t velocity = buffer[2];
+    velocity = velocity << 8 | buffer[3];
+    int16_t acceleration = buffer[4];
+    acceleration = acceleration << 8 | buffer[5];
+    int16_t current = buffer[6];
+    current = current << 8 | buffer[7];
+    motor.status.temperature = buffer[8];
+    motor.status.booleans = buffer[9];
+
+    // revert motor status data multipication to have the float number
+    motor.status.angle = angle / 10.0;
+    motor.status.velocity = velocity / 100.0;
+    motor.status.acceleration = acceleration / 100.0;
+    motor.status.current = current;
+
+    // read individual booleans from motor status boolean pack
+    bool rotatingCW = (motor.status.booleans) & 1;   // read the first bit
+    bool isAccelerating = (motor.status.booleans >> 1) & 1;   // read the second bit
+
+    Serial.print(motor.status.angle);Serial.print('\t');
+    Serial.print(motor.status.velocity);Serial.print('\t');
+    //Serial.print(setpointAngle);Serial.print('\t');    // print setpoint for comparison
+    Serial.print('\n');
 }
 
 void setup() {
